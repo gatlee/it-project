1c2d8277c91e6d7e1d2a3fbb5a21d4b9eedac9d3
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-08-15 19:07:30 +1000

    Implement local server-client connection

--- backend/index.ts
+++ backend/index.ts
@@ -22,10 +22,16 @@ app.get('/api/passwords', (req, res) => {
     console.log(`Sent ${count} passwords`);
 });
 
+app.get('/api/healthcheck', (req, res) => {
+    res.send({
+        "body": "OK"
+    });
+});
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'..frontend/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState("FAIL");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/healthcheck");
+      const json = await res.json();
+      setData(json.body);
+    };
+    fetchData().then(null).catch((err) => console.log(err));
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Express Connection Status: {data}
         </p>
         <a
           className="App-link"
