fe26e717c5a6c03b659f524e48264c0ad549400a
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-08-15 20:10:42 +1000

    Fix express middleware not delivering static files

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ const generatePassword = require('password-generator');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(process.cwd(), '../frontend/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
@@ -28,13 +28,12 @@ app.get('/api/healthcheck', (req, res) => {
     });
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'..frontend/build/index.html'));
+//Finally if nothing resolves send index.html
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(process.cwd(), '../frontend/build')});
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
