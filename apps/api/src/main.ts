7084b5cd5cd4f10b0d2a2e83408b1c50029cb5fb
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-08-22 16:52:46 +1000

    Implement heroku build and launch scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,20 @@
 import * as express from 'express';
+import * as path from 'path';
 import { Message } from '@pure-and-lazy/api-interfaces';
 
 const app = express();
+
+app.use(express.static(path.join(process.cwd(), '/dist/apps/client')));
 
 const greeting: Message = { message: 'Welcome to api!' };
 
 app.get('/api', (req, res) => {
   res.send(greeting);
+});
+
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(process.cwd(), '/dist/apps/client')});
 });
 
 const port = process.env.PORT || 3001;
