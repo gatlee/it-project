eedde672fd75e6b5c6032181272a47dd097c3eb2
James Dyer <jamesdyer199@gmail.com>
2020-08-25 23:25:33 +1000

    Split up PortfolioItem using a discriminator to express different item types

--- apps/api/src/models/portfolioItem.ts
+++ apps/api/src/models/portfolioItem.ts
@@ -1,9 +1,27 @@
 import * as mongoose from 'mongoose';
 
+const options = { discriminatorKey: 'itemType' };
+
+const PortfolioItem = mongoose.model(
+  'PortfolioItem',
+  new mongoose.Schema(
+    {
+      id: {
+        type: mongoose.Types.ObjectId,
+        index: true,
+      },
+      name: String,
+      lastModified: Date,
+    },
+    options
+  )
+);
+
 // For now, portfolio items are just plain text
 // TODO: implement images, document files e.g. PDF, rich text documents with associated images
-const portfolioItemSchema = new mongoose.Schema({
-  content: String,
-});
+const TextPortfolioItem = PortfolioItem.discriminator(
+  'TextPortfolioItem',
+  new mongoose.Schema({ content: String }, options)
+);
 
-export default mongoose.model('PortfolioItem', portfolioItemSchema);
+export { PortfolioItem, TextPortfolioItem };
