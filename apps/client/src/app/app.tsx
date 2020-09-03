4bca3d951548950e3da432ba2d3764fe3a704b09
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-09-03 14:28:05 +1000

    Add bootstrap css
    As per https://react-bootstrap.github.io/getting-started/introduction/

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = () => {
   return (
