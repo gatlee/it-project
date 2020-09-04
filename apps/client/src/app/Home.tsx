2e5361f583bad57d8393e633934457f5afae6709
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-05 02:52:18 +1000

    Add SignOutButton.tsx and a sign-in status message to Home.tsx

--- apps/client/src/app/Home.tsx
+++ apps/client/src/app/Home.tsx
@@ -3,9 +3,12 @@ import { Message } from '@pure-and-lazy/api-interfaces';
 import { Link } from 'react-router-dom';
 import GetStartedButton from './buttons/GetStartedButton';
 import SignInButton from './buttons/SignInButton';
+import SignOutButton from './buttons/SignOutButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const Home = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
+  const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
     fetch('/api')
@@ -26,7 +29,11 @@ export const Home = () => {
           <h2>Click here to be routed to foo's portfolio!</h2>
         </Link>
         <SignInButton />
+        <SignOutButton />
         <GetStartedButton />
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        {isAuthenticated ? 'Signed In' : 'Not Signed In'}
       </div>
       <div>{m.message}</div>
     </>
