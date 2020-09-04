6ff775a8998f2c5ba956aad54d1cb00310d8d0e3
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-04 12:11:34 +1000

    Add background for portfolio page

--- apps/client/src/app/portfolio-shared/portfolio-home.tsx
+++ apps/client/src/app/portfolio-shared/portfolio-home.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { PortfolioNavBar } from './portfolio-nav-bar';
+import CoolBackground from '../../assets/CoolBackground.png';
 
 const PortfolioHome = () => {
   const { id } = useParams();
 
+  const backgroundContainer = {
+    backgroundImage: `url(${CoolBackground})`,
+    height: '100vh',
+    width: '100vw',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
+
   return (
-    <>
+    <div style={backgroundContainer}>
       <PortfolioNavBar />
       <Container>
         <Row>
           <h1>{id}'s ePortfolio</h1>
         </Row>
       </Container>
-    </>
+    </div>
   );
 };
 
