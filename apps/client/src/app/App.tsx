59147a218edf9a0de4e1daf8e07eb551cf257bc5
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-05 01:51:15 +1000

    create Auth0ProviderWithHistory component and add it to App.tsx

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -5,21 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from './signup';
 import { PortfolioIndex } from './portfolio-shared/PortfolioIndex';
 
+import Auth0ProviderWithHistory from './Auth0ProviderWithHistory';
 
 export const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/u/:id">
-          <PortfolioIndex />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Auth0ProviderWithHistory>
+        <Switch>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/u/:id">
+            <PortfolioIndex />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Auth0ProviderWithHistory>
     </Router>
   );
 };
