895047fcdced1c9f4bde28a223daea66978d7b5a
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-04 13:14:29 +1000

    Correct merge of PortfolioHome

--- apps/client/src/app/portfolio-shared/PortfolioHome.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioHome.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { PortfolioNavBar } from './portfolio-nav-bar';
+import CoolBackground from '../../assets/CoolBackground.png';
+import { BackgroundContainer } from '../BackgroundContainer';
 
 const PortfolioHome = () => {
   const { id } = useParams();
+
   return (
-    <Container>
-      <Row>
-        <h1>{id}'s ePortfolio Home</h1>
-      </Row>
-    </Container>
+    <BackgroundContainer background={CoolBackground}>
+      <PortfolioNavBar />
+      <Container>
+        <Row>
+          <h1>{id}'s ePortfolio</h1>
+        </Row>
+      </Container>
+    </BackgroundContainer>
   );
 };
+
 export { PortfolioHome };
