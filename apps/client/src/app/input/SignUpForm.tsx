6d66a109f5775ab02f2ebccbeb7622264b19a52e
Matt <gamma032@gmail.com>
2020-09-04 12:40:19 +1000

    Swap username for full name

--- apps/client/src/app/input/SignUpForm.tsx
+++ apps/client/src/app/input/SignUpForm.tsx
@@ -4,28 +4,26 @@ import SignUpButton from '../buttons/SignUpButton';
 
 const SignUpForm = () => {
   return (
-    <Form>
-      <Col md={{ span: 4, offset: 4 }}>
-        <Form.Group controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control placeholder="Username" />
-        </Form.Group>
+    <Form as={Col} md={{ span: 4, offset: 4 }}>
+      <Form.Group controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control placeholder="Username" />
+      </Form.Group>
 
-        <Form.Group controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <Form.Group controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
 
-        <SignUpButton />
-      </Col>
+      <SignUpButton />
     </Form>
   );
 };
