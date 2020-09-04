f22558b6a7cb1f79fb94c505e9f9fbf337695660
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-04 13:19:43 +1000

    Move BackgroundContainer to PortfolioIndex

--- apps/client/src/app/portfolio-shared/PortfolioHome.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioHome.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import { PortfolioNavBar } from './portfolio-nav-bar';
-import CoolBackground from '../../assets/CoolBackground.png';
-import { BackgroundContainer } from '../BackgroundContainer';
 
 const PortfolioHome = () => {
   const { id } = useParams();
 
   return (
-    <BackgroundContainer background={CoolBackground}>
-      <PortfolioNavBar />
-      <Container>
-        <Row>
-          <h1>{id}'s ePortfolio</h1>
-        </Row>
-      </Container>
-    </BackgroundContainer>
+    <Container>
+      <Row>
+        <h1>{id}'s ePortfolio</h1>
+      </Row>
+    </Container>
   );
 };
 
