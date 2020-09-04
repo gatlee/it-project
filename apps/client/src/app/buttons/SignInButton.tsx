6af01c31160071a6b5d954dc935549ddb994d13b
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-05 02:28:26 +1000

    Add Auth0 functionality to SignInButton.tsx

--- apps/client/src/app/buttons/SignInButton.tsx
+++ apps/client/src/app/buttons/SignInButton.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
+
 const SignInButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <Button style={{ background: '#E5E5E5' }} variant="light">
+    <Button
+      style={{ background: '#E5E5E5' }}
+      variant="light"
+      onClick={() => loginWithRedirect()}
+    >
       Sign In
     </Button>
   );
