80c1c48b004b08f37a3cc39be4b138ef56b92690
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-05 17:00:15 +1000

    Implement edit footer into portfolio index page

--- apps/client/src/app/portfolio-shared/PortfolioIndex.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioIndex.tsx
@@ -6,28 +6,57 @@ import { useRouteMatch } from 'react-router-dom';
 import { PortfolioPortfolio } from './PortfolioPortfolio';
 import CoolBackground from '../../assets/CoolBackground.png';
 import { BackgroundContainer } from '../BackgroundContainer';
+import { PortfolioEditFooter } from './PortfolioEditFooter';
+import { Container } from 'react-bootstrap';
 
 const PortfolioIndex = () => {
   const { path } = useRouteMatch();
 
+  const isEditMode = true;
+
   return (
-    <BackgroundContainer background={CoolBackground}>
-      <PortfolioNavBar />
-      <Switch>
-        <Route exact path={`${path}`}>
-          <PortfolioHome />
-        </Route>
-        <Route exact path={`${path}/portfolio`}>
-          <PortfolioPortfolio />
-        </Route>
-        <Route exact path={`${path}/blog`}>
-          <h1>Blog</h1>
-        </Route>
-        <Route exact path={`${path}/about`}>
-          <h1>About</h1>
-        </Route>
-      </Switch>
-    </BackgroundContainer>
+    <>
+      <BackgroundContainer background={CoolBackground}>
+        <PortfolioWrapper>
+          <div style={{ flex: 1 }}>
+            <PortfolioNavBar />
+            <Switch>
+              <Route exact path={`${path}`}>
+                <PortfolioHome />
+              </Route>
+              <Route exact path={`${path}/portfolio`}>
+                <PortfolioPortfolio />
+              </Route>
+              <Route exact path={`${path}/blog`}>
+                <h1>Blog</h1>
+              </Route>
+              <Route exact path={`${path}/about`}>
+                <h1>About</h1>
+              </Route>
+            </Switch>
+          </div>
+          {isEditMode && <PortfolioEditFooter />}
+        </PortfolioWrapper>
+      </BackgroundContainer>
+    </>
+  );
+};
+
+interface PortfolioWrapper {
+  children: React.ReactNode;
+}
+
+const PortfolioWrapper = (props: PortfolioWrapper) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        minHeight: '100vh',
+        flexDirection: 'column',
+      }}
+    >
+      {props.children}
+    </div>
   );
 };
 
