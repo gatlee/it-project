ceb6fbc79075eae6300552f3ddc175808e0e70fa
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-05 16:12:25 +1000

    Add basic scaffolding for image preloading

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from './signup';
 import { PortfolioIndex } from './portfolio-shared/PortfolioIndex';
+import CoolBackground from '../assets/CoolBackground.png';
+import GradientBackground from '../assets/GradientBackground.png';
 
 export const App = () => {
+  // Backgrounds for home page and portfolio currently
+  const imagesToPreload = [CoolBackground, GradientBackground];
+
+  const cacheImages = () => {
+    imagesToPreload.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  };
+
+  useEffect(() => {
+    cacheImages();
+  }, []);
+
   return (
     <Router>
       <Switch>
