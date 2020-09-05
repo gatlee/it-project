6700cacc9373717a3391ed5f7493f4e9b5f99d57
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-05 17:20:39 +1000

    Fix typeScript lint type issue

--- apps/client/src/app/portfolio-shared/PortfolioEditFooter.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioEditFooter.tsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 interface PortfolioEditFooter {
-  user: String;
+  user: string;
   handleViewPublic: () => void;
 }
 
