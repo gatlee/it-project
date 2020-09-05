79c315438d209749fde30fa1dc09c0adf15a350f
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-05 23:49:28 +1000

    Revert "Implement Auth0 to the frontend"

--- apps/client/src/app/Home.tsx
+++ apps/client/src/app/Home.tsx
@@ -3,14 +3,11 @@ import { Message } from '@pure-and-lazy/api-interfaces';
 import { Link } from 'react-router-dom';
 import GetStartedButton from './buttons/GetStartedButton';
 import SignInButton from './buttons/SignInButton';
-import SignOutButton from './buttons/SignOutButton';
-import { useAuth0 } from '@auth0/auth0-react';
 import GradientBackground from '../assets/GradientBackground.png';
 import { BackgroundContainer } from './BackgroundContainer';
 
 export const Home = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
-  const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
     fetch('/api')
@@ -33,12 +30,9 @@ export const Home = () => {
         <h2>Click here to be routed to foo's portfolio!</h2>
       </Link>
       <SignInButton />
-      <SignOutButton />
       <GetStartedButton />
-      <div>{isAuthenticated ? 'Signed In' : 'Not Signed In'}</div>
       <div>{m.message}</div>
     </BackgroundContainer>
   );
 };
-
 export default Home;
--- apps/client/src/app/buttons/SignInButton.tsx
+++ apps/client/src/app/buttons/SignInButton.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { useAuth0 } from '@auth0/auth0-react';
-
 const SignInButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
   return (
-    <Button
-      style={{ background: '#E5E5E5' }}
-      variant="light"
-      onClick={() => loginWithRedirect()}
-    >
+    <Button style={{ background: '#E5E5E5' }} variant="light">
       Sign In
     </Button>
   );
