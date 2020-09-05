8e45a89bf257fc8b37b5ad80837dbce8dad14254
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-05 19:12:06 +1000

    Remove console.log() from Auth0ProviderWithHistory.tsx

--- apps/client/src/app/Auth0ProviderWithHistory.tsx
+++ apps/client/src/app/Auth0ProviderWithHistory.tsx
@@ -10,7 +10,6 @@ const Auth0ProviderWithHistory = ({ children }) => {
   // Regarding environment variable naming: https://nx.dev/react/guides/environment-variables
   const domain = process.env.NX_AUTH0_DOMAIN;
   const clientId = process.env.NX_AUTH0_CLIENT_ID;
-  console.log(domain, clientId);
 
   const history = useHistory();
 
