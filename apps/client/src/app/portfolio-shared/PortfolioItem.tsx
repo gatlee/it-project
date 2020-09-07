ebb01d572f18144798610b7e6ec41e6de84daada
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-09-07 21:40:04 +1000

    Add dummy edit icon

--- apps/client/src/app/portfolio-shared/PortfolioItem.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Pencil } from 'react-bootstrap-icons';
 
 interface PortfolioItem {
   title: string;
@@ -7,10 +8,25 @@ interface PortfolioItem {
 }
 
 const PortfolioItem = (props: PortfolioItem) => {
+  const outerStyle = {
+    backgroundColor: 'white',
+    marginTop: '20px',
+    padding: '2vh',
+  };
+
   return (
-    <Container style={{ backgroundColor: 'white' }}>
-      <h1>{props.title}</h1>
-      <i>{props.description}</i>
+    <Container style={outerStyle}>
+      <Row sm={10}>
+        <Col>
+          <h1>{props.title}</h1>
+          <i>{props.description}</i>
+        </Col>
+        <Col sm={'auto'}>
+          <Container style={{ padding: '1vh' }}>
+            <Pencil />
+          </Container>
+        </Col>
+      </Row>
     </Container>
   );
 };
--- apps/client/src/app/portfolio-shared/PortfolioPortfolio.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioPortfolio.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { PortfolioItem } from './PortfolioItem';
-import BackgroundItem from '../../assets/portfoliobackground.png';
 
 const PortfolioPortfolio = () => {
-  const style = {
-    backgroundImage: `url(${BackgroundItem}`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    height: '100%',
-  };
   return (
-    <div style={style}>
-      <Container fluid>
-        <Row>
-          <PortfolioItem
-            title="Worked at McDonalds"
-            description="It was pretty cool I guess"
-          />
-        </Row>
-        <Row>
-          <PortfolioItem
-            title="Worked at a grocery store"
-            description="Not so cool but still kinda cool? "
-          />
-        </Row>
-      </Container>
-    </div>
-
+    <Container fluid>
+      <Row>
+        <PortfolioItem
+          title="Worked at McDonalds"
+          description="It was pretty cool I guess"
+        />
+      </Row>
+      <Row>
+        <PortfolioItem
+          title="Worked at a grocery store"
+          description="Not so cool but still kinda cool? "
+        />
+      </Row>
+    </Container>
   );
 };
 
