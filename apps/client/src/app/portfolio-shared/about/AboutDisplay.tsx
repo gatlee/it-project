5894b2d76ec82e93230170f286e14dde2389e46a
Steven Nguyen <stevenhuyn@gmail.com>
2020-09-08 22:32:35 +1000

    Fix styling and floated pencil right

--- apps/client/src/app/portfolio-shared/about/AboutDisplay.tsx
+++ apps/client/src/app/portfolio-shared/about/AboutDisplay.tsx
@@ -12,21 +12,16 @@ interface AboutDisplay {
 const AboutDisplay = (props: AboutDisplay) => {
   const pencilStyle: React.CSSProperties = {
     cursor: 'pointer',
+    float: 'right',
   };
 
   return (
-    <Row sm={10}>
-      <Col>
-        <ReactMarkdown source={props.about} />
-      </Col>
-      <Col sm="auto">
-        <Container style={{ padding: '1vh' }}>
-          {props.editable && (
-            <Pencil onClick={props.onOpenEditor} style={pencilStyle} />
-          )}
-        </Container>
-      </Col>
-    </Row>
+    <>
+      {props.editable && (
+        <Pencil onClick={props.onOpenEditor} style={pencilStyle} />
+      )}
+      <ReactMarkdown source={props.about} />
+    </>
   );
 };
 
