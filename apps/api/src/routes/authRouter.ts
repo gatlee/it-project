032a51794d11ecf1b5e25a93cc4b0f8b7fae8520
James Dyer <jamesdyer199@gmail.com>
2020-09-10 17:02:01 +1000

    Add an API route for deleting portfolio items

--- apps/api/src/routes/authRouter.ts
+++ apps/api/src/routes/authRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import * as authController from '../controller/authController';
+import { logIn, logOut, signUp } from '../controller/authController';
 
 const router = Router();
-router.get('/log-in', authController.logIn);
-router.get('/log-out', authController.logOut);
-router.get('/sign-up', authController.signUp);
+router.get('/log-in', logIn);
+router.get('/log-out', logOut);
+router.get('/sign-up', signUp);
 
 export default router;
--- apps/api/src/routes/portfolioRouter.ts
+++ apps/api/src/routes/portfolioRouter.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
-import * as portfolioController from '../controller/portfolioController';
+import {
+  viewAllItems,
+  createItem,
+  viewProfile,
+  editProfile,
+  viewItem,
+  editItem,
+  deleteItem,
+} from '../controller/portfolioController';
 
 const router = Router();
-router.get('/:username/all', portfolioController.viewAllItems);
-router.post('/:username/create', portfolioController.createItem);
-router.get('/:username/profile', portfolioController.viewProfile);
-router.put('/:username/profile', portfolioController.editProfile);
-router.get('/:username/:portfolioItemId', portfolioController.viewItem);
-router.put('/:username/:portfolioItemId', portfolioController.editItem);
+router.get('/:username/all', viewAllItems);
+router.post('/:username/create', createItem);
+router.get('/:username/profile', viewProfile);
+router.put('/:username/profile', editProfile);
+router.get('/:username/:portfolioItemId', viewItem);
+router.put('/:username/:portfolioItemId', editItem);
+router.delete('/:username/:portfolioItemId', deleteItem);
 
 export default router;
