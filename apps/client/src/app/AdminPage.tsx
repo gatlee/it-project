86354989d0076c29aa88fc46e0028d78496c0267
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-13 23:58:50 +1000

    Add "Edit Porfolio" button in AdminPage

--- apps/client/src/app/AdminPage.tsx
+++ apps/client/src/app/AdminPage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BackgroundContainer } from './BackgroundContainer';
 import GradientBackground from '../assets/GradientBackground.png';
 import { useAuth0 } from '@auth0/auth0-react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export const AdminPage = () => {
   const { user } = useAuth0();
-  const { name, picture, email } = user;
+  const { name, picture, email, nickname } = user;
 
   return (
     <BackgroundContainer
@@ -17,6 +19,9 @@ export const AdminPage = () => {
       <p>{email}</p>
       <h4>Other User Content:</h4>
       <p>{JSON.stringify(user, null, 2)}</p>
+      <LinkContainer to={`/u/${nickname}`}>
+        <Button>Edit Portfolio</Button>
+      </LinkContainer>
     </BackgroundContainer>
   );
 };
