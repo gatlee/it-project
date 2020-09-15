04042370d49325c165ff3b6e3fe7a8237328cb1f
Matt <gamma032@gmail.com>
2020-09-15 23:25:10 +1000

    Add mobile support

--- apps/client/src/app/homepage/PromotionBox.tsx
+++ apps/client/src/app/homepage/PromotionBox.tsx
@@ -1,37 +1,57 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Image } from 'react-bootstrap';
 import GetStartedButton from '../buttons/GetStartedButton';
 import SignInButton from '../buttons/SignInButton';
 import SignOutButton from '../buttons/SignOutButton';
+import DemoImage from '../../assets/PortfolioDemo.png';
+import { Justify } from 'react-bootstrap-icons';
+import { Auth0Context } from '@auth0/auth0-react';
+import './PromotionBoxStyles.css';
 
 const PromotionBox = () => {
+  const containerStyle = {
+    paddingTop: '10vh',
+  };
+
   const titleStyle = {
-    fontSize: '4em',
+    fontSize: '4rem',
     fontFamily: 'Segoe UI Semibold',
     padding: '1vh 0 1vh 0',
   };
 
   const subtitleStyle = {
-    fontSize: '2em',
-    paddingBottom: '5vh',
+    fontSize: '2rem',
+    paddingBottom: '8vh',
+  };
+
+  const imageStyle = {
+    maxWidth: '700px',
+    width: '100%',
+  };
+
+  const buttonStyle = {
+    display: 'inline',
   };
 
   return (
-    <Container className="justify-content-md-center pt-5" fluid>
+    <Container
+      className="d-flex justify-content-center"
+      style={containerStyle}
+      fluid
+    >
       <Row>
-        <Col md={6} sm={10} className="py-3">
+        <Col lg={6} md={12} className="py-3" id="title-column">
           <h1 style={titleStyle}>Pure &amp;&amp; Lazy</h1>
           <h2 style={subtitleStyle}>Create your own ePortfolio in minutes.</h2>
-          <SignInButton />
-          <SignOutButton />
-          <GetStartedButton />
+          <div id="buttons" style={buttonStyle}>
+            <SignInButton />
+            <SignOutButton />
+            <GetStartedButton />
+          </div>
         </Col>
-        <Col md={6} sm={10} className="py-3">
-          <img
-            width="250"
-            src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-            alt="nx logo"
-          />
+
+        <Col lg={6} md={12} className="py-3">
+          <Image src={DemoImage} style={imageStyle} fluid />
         </Col>
       </Row>
     </Container>
