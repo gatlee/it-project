ad63d9ec4b19012f85bc34fccca56ed00cf9a6c7
James Dyer <jamesdyer199@gmail.com>
2020-09-18 23:44:16 +1000

    Add swagger docs for the /api/auth/create-user route

--- apps/api/src/routes/authRouter.ts
+++ apps/api/src/routes/authRouter.ts
@@ -2,7 +2,45 @@ import { Router } from 'express';
 import { createUser } from '../controller/authController';
 
 const router = Router();
-// FIXME: authenticate that the request is coming from the Auth0 hook
+
+/**
+ * @swagger
+ * /create-user:
+ *   post:
+ *     description:
+ *       Create a new user.
+ *       Should only be called from the Auth0 post-registration rule to handle a newly-registered user.
+ *     parameters:
+ *       - name: userInfo
+ *         in: body
+ *         description: Information about the newly registered user.
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - username
+ *             - email
+ *             - auth0Id
+ *           properties:
+ *             username:
+ *               type: string
+ *             email:
+ *               type: string
+ *             auth0Id:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: The user's profile.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/UserProfile'
+ *       404:
+ *         description: Unknown user.
+ *     tags:
+ *       - /api/auth
+ */
+// FIXME: authenticate that the request is coming from the Auth0 rule
 router.post('/create-user', createUser);
 
 export default router;
