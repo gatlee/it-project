c849c36c24e8164e0ed079650fd6753f4135af0b
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-01 08:59:18 +1000

    Add a Youtube Command to the rich markdown editor

--- apps/client/src/app/projects/editor/EditorBody.tsx
+++ apps/client/src/app/projects/editor/EditorBody.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
-import ReactMde from 'react-mde';
+import ReactMde, { Command, getDefaultToolbarCommands } from 'react-mde';
 import ReactMarkdown from 'react-markdown/umd/react-markdown';
 import { generateCloudinaryUrl } from '../../cloudinaryUtility';
+import { PlayFill } from 'react-bootstrap-icons';
 
 interface EditorBody {
   content: string;
@@ -23,15 +24,34 @@ const EditorBody = (props: EditorBody) => {
     return true;
   };
 
+  const youtubeCommand: Command = {
+    buttonProps: { 'aria-label': 'Add Youtube Embed' },
+    icon: () => <PlayFill />,
+    execute: (opts) => {
+      const youtubeEmbed = `<div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{ID-GOES-HERE}" allowfullscreen></iframe>
+    </div>`;
+      opts.textApi.replaceSelection(youtubeEmbed);
+    },
+  };
+
+  const toolbarCommands = [...getDefaultToolbarCommands(), ['youtube-embed']];
+
   return (
     <Container fluid className="px-0 mx-0">
       <ReactMde
+        commands={{
+          'youtube-embed': youtubeCommand,
+        }}
+        toolbarCommands={toolbarCommands}
         value={props.content}
         onChange={props.onContentChange}
         selectedTab={selectedTab}
         onTabChange={setSelectedTab}
         generateMarkdownPreview={(markdown) =>
-          Promise.resolve(<ReactMarkdown source={markdown} />)
+          Promise.resolve(
+            <ReactMarkdown escapeHtml={false} source={markdown} />
+          )
         }
         paste={{
           saveImage: saveImage,
