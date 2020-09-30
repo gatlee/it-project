7f77acf3c7a686fc5dc8d8e9910fd50b0d438a60
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-09-30 17:30:10 +1000

    Implement parts of new user signup flow

--- apps/client/src/app/AdminPage.tsx
+++ apps/client/src/app/AdminPage.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BackgroundContainer } from './BackgroundContainer';
 import GradientBackground from '../assets/GradientBackground.png';
 import { useAuth0 } from '@auth0/auth0-react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Button } from 'react-bootstrap';
+import { Button, FormControl, InputGroup } from 'react-bootstrap';
+import LoadingScreen from './LoadingScreen';
 
 export const AdminPage = () => {
-  const { user } = useAuth0();
-  const { name, picture, email, nickname } = user;
+  const { user, getAccessTokenSilently } = useAuth0();
+  const { name, given_name, picture, email, nickname } = user;
+  const [userData, setUserData] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const auth0Domain = 'pure-and-lazy.au.auth0.com';
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${auth0Domain}/api/v2/`,
+          scope: 'read:current_user',
+        });
+        console.log('got access token');
+
+        const userDetailsByIdUrl = `https://${auth0Domain}/api/v2/users/${user.sub}`;
+
+        const userDataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        console.log('got user response');
+
+        setUserData(await userDataResponse.json());
+        setIsLoaded(true);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserData();
+  }, []);
+
+  // if (!isLoaded) {
+  //   return (
+  //     <LoadingScreen />
+  //   )
+  // }
 
   return (
     <BackgroundContainer
@@ -15,10 +55,27 @@ export const AdminPage = () => {
       style={{ textAlign: 'center' }}
     >
       <img src={picture} alt="Profile" style={{ maxWidth: '100px' }} />
-      <h2>{name}</h2>
+      {/*<h2>{name}</h2>*/}
       <p>{email}</p>
-      <h4>Other User Content:</h4>
-      <p>{JSON.stringify(user, null, 2)}</p>
+      {/*<h4>Other User Content:</h4>*/}
+      <p>{JSON.stringify(userData, null, 2)}</p>
+      <h2>Hi {given_name}, welcome to ePortfolio Maker by Pure && Lazy</h2>
+
+      <label htmlFor="public-url-form">
+        <h4>To get started, please decide on a public facing URL:</h4>
+      </label>
+      <InputGroup className="mb-3" style={{ width: '100%' }}>
+        <InputGroup.Prepend>
+          <InputGroup.Text id="base-url-prepend">
+            https://pure-and-lazy.herokuapp.com/u/
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl id="public-url-form" aria-describedby="base-url-prepend" />
+        <InputGroup.Append>
+          <Button variant="primary">Submit</Button>
+        </InputGroup.Append>
+      </InputGroup>
+
       <LinkContainer to={`/u/${nickname}`}>
         <Button>View Portfolio</Button>
       </LinkContainer>
