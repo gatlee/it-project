a0b2f640683a3c2f42db5ba95be2f3fefb65884c
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-10-01 18:39:19 +1000

    Update portfolio home design

--- apps/client/src/app/portfolio-shared/PortfolioHome.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioHome.tsx
@@ -4,25 +4,42 @@ import { PortfolioAvatar } from './PortfolioAvatar';
 import { CenteredRowContent } from '../layout/CenteredRowContent';
 import { UploadBox } from '../UploadBox';
 import { EditContext } from './EditContext';
+import { BackgroundContainer } from '../BackgroundContainer';
 
+import BackgroundImage from '../../assets/landscape.png';
+import { useAuth0 } from '@auth0/auth0-react';
 const PortfolioHome = () => {
+  const { user } = useAuth0();
+  const { name } = user;
   const editMode = useContext(EditContext);
 
   return (
-    <Container>
-      <Row>
-        <CenteredRowContent>
-          <PortfolioAvatar />
-        </CenteredRowContent>
-      </Row>
-      {editMode && (
+    <BackgroundContainer
+      background={BackgroundImage}
+      style={{
+        backgroundImage: ` linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${BackgroundImage})`,
+      }}
+    >
+      <Container>
         <Row>
           <CenteredRowContent>
-            <UploadBox />
+            <PortfolioAvatar />
           </CenteredRowContent>
         </Row>
-      )}
-    </Container>
+        <Row>
+          <CenteredRowContent>
+            <h1 className="text-white display-1 mt-5 text-center"> {name} </h1>
+          </CenteredRowContent>
+        </Row>
+        {editMode && (
+          <Row>
+            <CenteredRowContent>
+              <UploadBox />
+            </CenteredRowContent>
+          </Row>
+        )}
+      </Container>
+    </BackgroundContainer>
   );
 };
 
