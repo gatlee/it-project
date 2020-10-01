a85a357c15c085f5b1d95c454a933d143e5ea97c
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-02 02:58:30 +1000

    Implement responses for url/user conflict

--- apps/api/src/controller/authController.ts
+++ apps/api/src/controller/authController.ts
@@ -5,9 +5,20 @@ interface Req {
   body?: { username?: string; email?: string; auth0Id?: string };
 }
 
-const createUser = async (req: Req, res: Res<never>) => {
+const createUser = async (req: Req, res: Res<String>) => {
   if (req.body && req.body.username && req.body.email && req.body.auth0Id) {
     const { username, email, auth0Id } = req.body;
+
+    if (await UserModel.findOne({ username: username })) {
+      res.status(409);
+      res.send('username taken');
+      return;
+    } else if (await UserModel.findOne({ auth0Id: auth0Id })) {
+      res.status(409);
+      res.send('auth0Id conflict');
+      return;
+    }
+
     await UserModel.create({
       username,
       email,
--- apps/api/src/controller/controllerUtil.ts
+++ apps/api/src/controller/controllerUtil.ts
@@ -1,5 +1,6 @@
 interface Res<T> {
   send: (res: T) => void;
+  status: (res: number) => void;
   sendStatus: (code: number) => void;
 }
 
