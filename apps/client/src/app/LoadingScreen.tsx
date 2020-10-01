241c106da660649669549e367e47fbd9e8f59a3f
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-01 11:18:19 +1000

    Add cleaner loading screen

--- apps/client/src/app/LoadingScreen.tsx
+++ apps/client/src/app/LoadingScreen.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Container, Row, Image, Spinner } from 'react-bootstrap';
+import { CenteredRowContent } from './layout/CenteredRowContent';
 
-// A template loading screen
-export const LoadingScreen = () => {
-  return <h1>Loading..</h1>;
+const LoadingScreen = () => {
+  return (
+    <Container>
+      <Row>
+        <CenteredRowContent>
+          <Image
+            className="mt-5"
+            src="https://upload.wikimedia.org/wikipedia/commons/3/39/Lambda_lc.svg"
+            height="150"
+          />
+        </CenteredRowContent>
+      </Row>
+      <Row>
+        <CenteredRowContent>
+          <h1>Loading</h1>
+        </CenteredRowContent>
+      </Row>
+      <Row className="pt-5">
+        <CenteredRowContent>
+          <Spinner animation="border" />
+        </CenteredRowContent>
+      </Row>
+    </Container>
+  );
 };
 
-export default LoadingScreen;
+export { LoadingScreen };
