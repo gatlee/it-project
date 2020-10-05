16a2f9eaf488191dacaba529a0e4004667b70c2a
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-06 04:36:38 +1100

    Add auth0 rules to repository

---
+++ auth0Rules.js
@@ -0,0 +1,30 @@
+/**
+ * Indicate user needs to complete registration details and redirect to registration completion page
+ * @param user
+ * @param context
+ * @param callback
+ * @returns {*}
+ */
+function (user, context, callback) {
+  // Short-circuit if the user is signed up already, is using a refresh token,
+  // or is using silent auth
+  if (context.stats.loginsCount > 1 || context.protocol === 'oauth2-refresh-token' ||         context.request.query.prompt === 'none') {
+    return callback(null, user, context);
+  }
+  // Set redirect url
+  context.redirect = {
+    url: "https://pure-and-lazy.herokuapp.com/admin"
+  };
+  // Set registration status
+  user.user_metadata = user.user_metadata || {};
+  user.user_metadata.registration_complete = false;
+  console.log("updating user_metadata");
+  auth0.users.updateUserMetadata(user.user_id, user.user_metadata)
+    .then(function(){
+      callback(null, user, context);
+    })
+    .catch(function(err){
+      console.log(err.message);
+      callback(err);
+    });
+}
