1435266772d9d8c6de9a79ec4c7db9a391c84b14
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-13 03:21:22 +1100

    Refactor css into a function

--- apps/client/src/app/portfolio-shared/PortfolioThemePicker.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioThemePicker.tsx
@@ -10,34 +10,25 @@ interface PortfolioThemePicker {
 const PortfolioThemePicker = (props: PortfolioThemePicker) => {
   const handleClose = () => props.onHide();
 
-  const darkThemeButton = css({
-    width: '60px',
-    height: '60px',
-    borderRadius: '50%',
-    border: '4px solid #E3E3E3',
-    background: 'black',
-    ':hover': {
-      border: '4px solid gray',
-    },
-  });
-
-  const lightThemeButton = css({
-    width: '60px',
-    height: '60px',
-    borderRadius: '50%',
-    border: '4px solid #E3E3E3',
-    background: 'white',
-    ':hover': {
-      border: '4px solid gray',
-    },
-  });
+  const themeButtonStyle = (color: string) =>
+    css({
+      width: '60px',
+      height: '60px',
+      borderRadius: '50%',
+      border: '4px solid #E3E3E3',
+      background: `${color}`,
+      ':hover': {
+        border: '4px solid gray',
+        '-webkit-transition': '.3s ease',
+      },
+    });
 
   return (
     <Modal show={props.show} centered toggle onHide={handleClose}>
       <Modal.Body className="m-3">
         <h2 className="mb-3">Color Scheme</h2>
-        <button className={lightThemeButton + ' mr-3'} />
-        <button className={darkThemeButton} />
+        <button className={themeButtonStyle('white') + ' mr-3'} />
+        <button className={themeButtonStyle('black')} />
         <h2 className="mt-3">Theme Image</h2>
       </Modal.Body>
     </Modal>
