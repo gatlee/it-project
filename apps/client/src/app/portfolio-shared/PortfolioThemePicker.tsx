8b9517b2b16ec90e3d0ea9c615181387fcd4eaff
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-13 02:44:10 +1100

    Create color theme buttons

--- apps/client/src/app/portfolio-shared/PortfolioThemePicker.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioThemePicker.tsx
@@ -1,3 +1,4 @@
+import { css } from 'emotion';
 import React from 'react';
 import { Button, Modal } from 'react-bootstrap';
 
@@ -9,11 +10,35 @@ interface PortfolioThemePicker {
 const PortfolioThemePicker = (props: PortfolioThemePicker) => {
   const handleClose = () => props.onHide();
 
+  const darkThemeButton = css({
+    width: '60px',
+    height: '60px',
+    borderRadius: '50%',
+    border: '4px solid #E3E3E3',
+    background: 'black',
+    ':hover': {
+      border: '4px solid gray',
+    },
+  });
+
+  const lightThemeButton = css({
+    width: '60px',
+    height: '60px',
+    borderRadius: '50%',
+    border: '4px solid #E3E3E3',
+    background: 'white',
+    ':hover': {
+      border: '4px solid gray',
+    },
+  });
+
   return (
     <Modal show={props.show} centered toggle onHide={handleClose}>
-      <Modal.Body>
-        <h1>Color Scheme</h1>
-        <h1>Theme Image</h1>
+      <Modal.Body className="m-3">
+        <h2 className="mb-3">Color Scheme</h2>
+        <button className={lightThemeButton + ' mr-3'} />
+        <button className={darkThemeButton} />
+        <h2 className="mt-3">Theme Image</h2>
       </Modal.Body>
     </Modal>
   );
