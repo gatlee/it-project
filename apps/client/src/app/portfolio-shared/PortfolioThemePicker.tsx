20e613f91b60b5128a6920de50d918bdc07593bb
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-13 03:51:29 +1100

    Style modal picker images for desktop

--- apps/client/src/app/portfolio-shared/PortfolioThemePicker.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioThemePicker.tsx
@@ -1,6 +1,6 @@
 import { css } from 'emotion';
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Container, Modal, Col, Row } from 'react-bootstrap';
 
 interface PortfolioThemePicker {
   show: boolean;
@@ -23,13 +23,45 @@ const PortfolioThemePicker = (props: PortfolioThemePicker) => {
       },
     });
 
+  const themeStyle = css({
+    width: '200px',
+    height: '100px',
+    background: 'LightGray',
+    border: 'none',
+  });
+
+  const modalStyle = css({
+    width: '40vw',
+    maxWidth: 'none',
+  });
+
   return (
-    <Modal show={props.show} centered toggle onHide={handleClose}>
-      <Modal.Body className="m-3">
-        <h2 className="mb-3">Color Scheme</h2>
+    <Modal
+      show={props.show}
+      centered
+      onHide={handleClose}
+      dialogClassName={modalStyle}
+      aria-labelledby="example-custom-modal-styling-title"
+    >
+      <Modal.Body className="m-3" id="example-custom-modal-styling-title">
+        <h3 className="mb-3">Color Scheme</h3>
         <button className={themeButtonStyle('white') + ' mr-3'} />
         <button className={themeButtonStyle('black')} />
-        <h2 className="mt-3">Theme Image</h2>
+        <h3 className="mt-3 mb-3">Theme Image</h3>
+
+        <Container>
+          <Row>
+            <Col className="text-center mb-3">
+              <img className={themeStyle + ' shadow '} />
+            </Col>
+            <Col className="text-center mb-3">
+              <img className={themeStyle + ' shadow '} />
+            </Col>
+            <Col className="text-center mb-3">
+              <img className={themeStyle + ' shadow '} />
+            </Col>
+          </Row>
+        </Container>
       </Modal.Body>
     </Modal>
   );
