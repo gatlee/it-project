5a9ab2bf5490bacd922ab38c59ef1a789bdb85e3
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-15 13:35:07 +1100

    Add 404 test

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -2,4 +2,9 @@ describe('smoke', () => {
   it('visits the app', () => {
     cy.visit('/');
   });
+
+  it('handle unknown route', () => {
+    cy.visit('/idontexist');
+    cy.contain('404');
+  });
 });
