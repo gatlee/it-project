788022dfeada001c1a89ed57c6ddee2e4a72c346
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-15 16:38:59 +1100

    Add basic Cypress test to test suite

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -1,13 +1,5 @@
-import { getGreeting } from '../support/app.po';
-
 describe('portfolio', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to portfolio!');
+  it('1+1', () => {
+    expect(1 + 1).to.equal(2);
   });
 });
