ddb9101b0f15aea36c0ac22a510d91589a37839d
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-21 07:11:00 +1100

    Comment out unused lines

--- apps/client/src/app/Home.tsx
+++ apps/client/src/app/Home.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@pure-and-lazy/api-interfaces';
+import React from "react";
+// import { Message } from '@pure-and-lazy/api-interfaces';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { PromotionBox } from './homepage/PromotionBox';
@@ -8,15 +8,15 @@ import { BackgroundContainer } from './BackgroundContainer';
 import { Container } from 'react-bootstrap';
 
 export const Home = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
+  // const [m, setMessage] = useState<Message>({ message: '' });
   const { isAuthenticated, isLoading } = useAuth0();
   // useAuth0 Hook: https://auth0.github.io/auth0-react/globals.html#useauth0
 
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api')
+  //     .then((r) => r.json())
+  //     .then(setMessage);
+  // }, []);
 
   return isLoading ? null : (
     <Container className="d-flex flex-column min-vh-100 p-0" fluid>
@@ -26,7 +26,7 @@ export const Home = () => {
       >
         <PromotionBox />
         <div>{isAuthenticated ? 'Signed In' : 'Not Signed In'}</div>
-        <div>{m.message}</div>
+        {/*<div>{m.message}</div>*/}
         <Link to="/u/gkaw">
           <h2>Example</h2>
         </Link>
