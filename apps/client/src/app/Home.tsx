1dc8789f382af8e0702ce0551bfddfafeb34010f
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-21 07:12:55 +1100

    Update Home to rely on AuthContext

--- apps/client/src/app/Home.tsx
+++ apps/client/src/app/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from 'react';
 // import { Message } from '@pure-and-lazy/api-interfaces';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -6,11 +6,13 @@ import { PromotionBox } from './homepage/PromotionBox';
 import GradientBackground from '../assets/GradientBackground.png';
 import { BackgroundContainer } from './BackgroundContainer';
 import { Container } from 'react-bootstrap';
+import { AuthContext } from './auth/AuthContext';
 
 export const Home = () => {
   // const [m, setMessage] = useState<Message>({ message: '' });
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated } = useAuth0();
   // useAuth0 Hook: https://auth0.github.io/auth0-react/globals.html#useauth0
+  const { isLoaded } = useContext(AuthContext);
 
   // useEffect(() => {
   //   fetch('/api')
@@ -18,7 +20,7 @@ export const Home = () => {
   //     .then(setMessage);
   // }, []);
 
-  return isLoading ? null : (
+  return isLoaded ? (
     <Container className="d-flex flex-column min-vh-100 p-0" fluid>
       <BackgroundContainer
         background={GradientBackground}
@@ -32,7 +34,7 @@ export const Home = () => {
         </Link>
       </BackgroundContainer>
     </Container>
-  );
+  ) : null;
 };
 
 export default Home;
