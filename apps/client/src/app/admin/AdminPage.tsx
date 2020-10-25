2f39cb6578cfe287a039449d1e7cc8f51caa13cb
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-10-25 18:45:26 +1100

    Fix linting issues

--- apps/client/src/app/admin/AdminPage.tsx
+++ apps/client/src/app/admin/AdminPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { BackgroundContainer } from '../BackgroundContainer';
 import GradientBackground from '../../assets/GradientBackground.png';
