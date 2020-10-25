37a778c1bbe49333f8a28301d425288ba310f842
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-10-25 20:46:51 +1100

    Bring NavBar elements closer to center of screen

--- apps/client/src/app/portfolio-shared/PortfolioNavBar.tsx
+++ apps/client/src/app/portfolio-shared/PortfolioNavBar.tsx
@@ -1,6 +1,6 @@
 import { css } from 'emotion';
 import React, { useContext } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useParams } from 'react-router-dom';
 import { EditContext } from './EditContext';
@@ -27,28 +27,30 @@ const PortfolioNavBar = () => {
       collapseOnSelect
       className="shadow"
     >
-      <LinkContainer
-        to={`${URL_PREFIX}/`}
-        className={css`
-          ${logoStyle}
-        `}
-      >
-        <Navbar.Brand>{name}</Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto" activeKey="blog">
-          <LinkContainer to={`${URL_PREFIX}/projects`}>
-            <Nav.Link eventKey="/projects">Projects</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to={`${URL_PREFIX}/blog`}>
-            <Nav.Link eventKey="/blog">Blog</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to={`${URL_PREFIX}/about`}>
-            <Nav.Link eventKey="/about">About</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <LinkContainer
+          to={`${URL_PREFIX}/`}
+          className={css`
+            ${logoStyle}
+          `}
+        >
+          <Navbar.Brand>{name}</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto" activeKey="blog">
+            <LinkContainer to={`${URL_PREFIX}/projects`}>
+              <Nav.Link eventKey="/projects">Projects</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to={`${URL_PREFIX}/blog`}>
+              <Nav.Link eventKey="/blog">Blog</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to={`${URL_PREFIX}/about`}>
+              <Nav.Link eventKey="/about">About</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
