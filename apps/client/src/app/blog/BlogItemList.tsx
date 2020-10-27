ec11ff420e4560e7e5dbfda74edb4ba7f9cc3ce8
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-10-27 16:39:02 +1100

    Remove redundant BlogItemLIst and ProjectItemList
    For simplification purposes

--- apps/client/src/app/blog/BlogItemList.tsx
+++
@@ -1,9 +0,0 @@
-import { PortfolioCategory } from '@pure-and-lazy/api-interfaces';
-import React from 'react';
-import { ItemList } from '../projects/ItemList';
-
-const BlogItemList = () => {
-  return <ItemList category={PortfolioCategory.BLOG} />;
-};
-
-export { BlogItemList };
--- apps/client/src/app/projects/ProjectItemList.tsx
+++
@@ -1,9 +0,0 @@
-import { PortfolioCategory } from '@pure-and-lazy/api-interfaces';
-import React from 'react';
-import { ItemList } from './ItemList';
-
-const ProjectItemList = () => {
-  return <ItemList category={PortfolioCategory.PROJECTS} />;
-};
-
-export { ProjectItemList };
