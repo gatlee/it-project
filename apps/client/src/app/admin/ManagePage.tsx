9fd986d95f0b691887db13218fb232e020fe1ab4
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-27 16:16:33 +1100

    Add title, sign out button and background to page

--- apps/client/src/app/admin/ManagePage.tsx
+++ apps/client/src/app/admin/ManagePage.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { BackgroundContainer } from '../BackgroundContainer';
+import { AdminSignOut } from './AdminSignOut';
+import GradientBackground from '../../assets/GradientBackground.png';
+import { AdminTitle } from './AdminTitle';
+import { Col, Container, Row } from 'react-bootstrap';
+import { css } from 'emotion';
 
 // Manage Public Information Page
 
 const ManagePage = () => {
-  return <h1>SUP!</h1>;
+  const topMarginStyle = css({
+    marginTop: '20vh',
+  });
+
+  return (
+    <BackgroundContainer background={GradientBackground}>
+      <div className="m-3 text-right">
+        <AdminSignOut />
+      </div>
+      <Container>
+        <Row className={topMarginStyle}></Row>
+        <Row>
+          <Col>
+            <AdminTitle
+              title="Edit Public Information"
+              subtitle="What details would you like to change?"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </BackgroundContainer>
+  );
 };
 
 export { ManagePage };
