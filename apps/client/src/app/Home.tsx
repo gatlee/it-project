f384ebbf4da971d66a79f947c28da888fd986bb9
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-10-27 16:26:25 +1100

    Remove unneeded api calls on homepage

--- apps/client/src/app/Home.tsx
+++ apps/client/src/app/Home.tsx
@@ -1,22 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@pure-and-lazy/api-interfaces';
-import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { PromotionBox } from './homepage/PromotionBox';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 import GradientBackground from '../assets/GradientBackground.png';
 import { BackgroundContainer } from './BackgroundContainer';
-import { Container } from 'react-bootstrap';
+import { PromotionBox } from './homepage/PromotionBox';
 
 export const Home = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isLoading } = useAuth0();
   // useAuth0 Hook: https://auth0.github.io/auth0-react/globals.html#useauth0
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
 
   return isLoading ? null : (
     <Container className="d-flex flex-column min-vh-100 p-0" fluid>
