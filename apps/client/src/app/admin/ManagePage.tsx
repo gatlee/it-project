8161ff1a50fb3584d37e573c4a38766b0f5152a1
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-27 19:08:37 +1100

    Add name field to page

--- apps/client/src/app/admin/ManagePage.tsx
+++ apps/client/src/app/admin/ManagePage.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BackgroundContainer } from '../BackgroundContainer';
 import { AdminSignOut } from './AdminSignOut';
 import GradientBackground from '../../assets/GradientBackground.png';
 import { AdminTitle } from './AdminTitle';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 import { css } from 'emotion';
 
 // Manage Public Information Page
 
 const ManagePage = () => {
+  const [name, setName] = useState('');
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setName(event.target.value);
+  };
+
   const topMarginStyle = css({
     marginTop: '20vh',
   });
@@ -26,6 +32,20 @@ const ManagePage = () => {
               title="Edit Public Information"
               subtitle="What details would you like to change?"
             />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={6} md={8}>
+            <Form onSubmit={() => alert('Functionality Not Done Yet')}>
+              <Form.Group className="mt-2" controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  onChange={handleNameChange}
+                  type="text"
+                  placeholder="Enter name"
+                />
+              </Form.Group>
+            </Form>
           </Col>
         </Row>
       </Container>
