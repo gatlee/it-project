37bf2bddf1bc2782d30100e099766ff302b8ee43
simon-nctran <53035234+simon-nctran@users.noreply.github.com>
2020-10-27 20:15:15 +1100

    Remove redundant lines

--- apps/client/src/app/Home.tsx
+++ apps/client/src/app/Home.tsx
@@ -1,7 +1,4 @@
 import React, { useContext } from 'react';
-// import { Message } from '@pure-and-lazy/api-interfaces';
-import { Link } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 import { PromotionBox } from './homepage/PromotionBox';
 import GradientBackground from '../assets/GradientBackground.png';
 import { BackgroundContainer } from './BackgroundContainer';
@@ -9,16 +6,8 @@ import { Container } from 'react-bootstrap';
 import { AuthContext } from './auth/AuthContext';
 
 export const Home = () => {
-  // const [m, setMessage] = useState<Message>({ message: '' });
-  const { isAuthenticated } = useAuth0();
   // useAuth0 Hook: https://auth0.github.io/auth0-react/globals.html#useauth0
   const { isLoaded } = useContext(AuthContext);
-
-  // useEffect(() => {
-  //   fetch('/api')
-  //     .then((r) => r.json())
-  //     .then(setMessage);
-  // }, []);
 
   return isLoaded ? (
     <Container className="d-flex flex-column min-vh-100 p-0" fluid>
